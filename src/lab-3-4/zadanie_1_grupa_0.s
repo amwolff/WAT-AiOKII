83bc1328cf40ea2c877467e04635cfc1bf478757
Artur M. Wolff <artur.m.wolff@gmail.com>
2018-03-11 11:34:08 +0100

    Added lab 3-4/zad 1-2 and docs

---
+++ src/lab-3-4/zadanie_1_grupa_0.s
@@ -0,0 +1,46 @@
+; 81₈, 110₁, 119₃, 151₅, 169₆, 175₇, 205₄, 236₁, 239₂, 251₂, 266₉, 279₉, 282₉
+
+.data
+			.word 81
+			.word 119
+			.word 151
+			.word 169
+			.word 175
+			.word 205
+	first:	.word 239
+	second:	.word 251
+	Table:	.word 266
+			.word 279
+			.word 282
+
+.text
+addi	r21, r0, 0
+lw		r1, 4096(r21)
+
+addi	r2, r0, 110
+
+lw		first-20
+
+lw		r4, 4104 ; 4096 + 8 = 4104
+
+lw		r5, 0x100C ; (4096 + 12)h = 0x100C
+
+addi	r20, r0, 16
+lw		r6, 4096(r20)
+
+lw		r7, first-0x4 ; (4)h = 0x4
+
+addi	r8, r0, 236
+
+lw		r9, first
+
+lw		r10, second
+
+addi	r22, r0, 0
+lw		r11, Table(r22)
+
+addi	r22, r0, 4
+lw		r12, Table(r22)
+
+addi	r22, r0, 4
+lw		r13, Table(r22)
---
+++ src/lab-3-4/zadanie_2_grupa_0.s
@@ -0,0 +1,69 @@
+; 81, 110, 119, 151, 169, 175, 205, 236, 239, 251, 266, 279, 282
+
+.data
+	HelloText:		.asciiz "Witaj!\nProgram zostal napisany przez wyrobnika z grupy H7X2S1.\nMoj numer stanowiska: 404.\n"
+	NumbersText:	.asciiz "Suma: %d\nRoznica: %d\nIloczyn: %d\nSrednia: %d\n"
+
+	.align 2
+	HelloTextPtr:	.word HelloText
+	NumbersTextPtr: .word NumbersText
+	Total: 			.word 0
+	Difference: 	.word 0
+	Product:		.word 0
+	Mean:			.word 0
+
+.text
+	addi r14, r0, HelloTextPtr
+	trap 5
+
+	addi r1, r0, 81
+	addi r2, r0, 110
+	addi r3, r0, 119
+	addi r4, r0, 151
+	addi r5, r0, 169
+	addi r6, r0, 175
+	addi r7, r0, 205
+	addi r8, r0, 236
+	addi r9, r0, 239
+	addi r10, r0, 251
+	addi r11, r0, 266
+	addi r12, r0, 279
+	addi r13, r0, 282
+
+	add r15, r4, r5
+	add r15, r15, r9
+
+	sw Total, r15
+
+	sub r16, r12, r6
+	sub r16, r16, r13
+
+	sw Difference, r16
+
+	movi2fp f1, r4
+	movi2fp f2, r8
+	movi2fp f3, r13
+
+	mult f4, f1, f2
+	mult f4, f4, f3
+
+	movfp2i r17, f4
+	sw Product, r17
+
+	add r18, r1, r4
+	add r18, r18, r11
+
+	movi2fp f5, r18
+
+	addi r19, r0, 3
+	movi2fp f6, r19
+
+	div f7, f5, f6
+
+	movfp2i r20, f7
+	sw Mean, r20
+
+	addi r14, r0, NumbersTextPtr
+	trap 5
+
+	trap 0
