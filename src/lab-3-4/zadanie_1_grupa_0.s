817672fb3cc73483cff88033a0cbc484a9551ed8
Artur M. Wolff <artur.m.wolff@gmail.com>
2018-03-11 15:59:11 +0100

    Fixed indentation and added trap 0's

--- src/lab-3-4/zadanie_1_grupa_0.s
+++ src/lab-3-4/zadanie_1_grupa_0.s
@@ -14,33 +14,35 @@
 			.word 282
 
 .text
-addi	r21, r0, 0
-lw		r1, 4096(r21)
+	addi	r21, r0, 0
+	lw		r1, 4096(r21)
 
-addi	r2, r0, 110
+	addi	r2, r0, 110
 
-lw		first-20
+	lw		first-20
 
-lw		r4, 4104 ; 4096 + 8 = 4104
+	lw		r4, 4104 ; 4096 + 8 = 4104
 
-lw		r5, 0x100C ; (4096 + 12)h = 0x100C
+	lw		r5, 0x100C ; (4096 + 12)h = 0x100C
 
-addi	r20, r0, 16
-lw		r6, 4096(r20)
+	addi	r20, r0, 16
+	lw		r6, 4096(r20)
 
-lw		r7, first-0x4 ; (4)h = 0x4
+	lw		r7, first-0x4 ; (4)h = 0x4
 
-addi	r8, r0, 236
+	addi	r8, r0, 236
 
-lw		r9, first
+	lw		r9, first
 
-lw		r10, second
+	lw		r10, second
 
-addi	r22, r0, 0
-lw		r11, Table(r22)
+	addi	r22, r0, 0
+	lw		r11, Table(r22)
 
-addi	r22, r0, 4
-lw		r12, Table(r22)
+	addi	r22, r0, 4
+	lw		r12, Table(r22)
 
-addi	r22, r0, 4
-lw		r13, Table(r22)
+	addi	r22, r0, 4
+	lw		r13, Table(r22)
+
+	trap	0
---
+++ src/lab-3-4/zadanie_1_grupa_4.s
@@ -0,0 +1,48 @@
+; 0₆, 18₁, 57₅, 63₄, 86₁, 97₇, 137₂, 180₈, 183₂, 259₃, 284₉, 284₉, 299₉
+
+.data
+			.word 0
+			.word 57
+			.word 63
+			.word 97
+	first:	.word 137
+			.word 180
+	second: .word 183
+			.word 259
+	Table:	.word 284
+			.word 284
+			.word 299
+
+.text
+	lw 		r1, 0x1000 ; (4096)h = 0x1000
+
+	addi 	r2, r0, 18
+
+	lw 		r3, 4100 ; 4096 + 4 = 4100
+
+	lw 		r4, first-0x8 ; (8)h = 0x8
+
+	addi 	r5, r0, 86
+
+	addi 	r20, r0, 12
+	lw 		r6, 4096(r20)
+
+	lw 		r7, first
+
+	addi 	r21, r0, 20
+	lw 		r8, 4096(r21)
+
+	lw 		r9, second
+
+	lw 		r10, second+4
+
+	addi 	r22, r0, 0
+	lw 		r11, Table(r22)
+
+	addi 	r22, r0, 4
+	lw 		r12, Table(r22)
+
+	addi 	r22, r0, 4
+	lw 		r13, Table(r22)
+
+	trap	0
