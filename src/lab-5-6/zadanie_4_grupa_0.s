9f0238b52275be395d138225fc8141d93f69b6fb
Artur M. Wolff <artur.m.wolff@gmail.com>
2018-04-01 01:18:29 +0200

    Added work-in-progress group 0 for ex. 4

---
+++ src/lab-5-6/zadanie_4_grupa_0.s
@@ -0,0 +1,29 @@
+.data
+	HelloText:      .asciiz "Witaj!\nProgram zostal napisany przez wyrobnika z grupy H7X2S1.\nMoj numer stanowiska: 404.\n"
+	ActionText:     .asciiz "Prosze o podanie znaku:"
+
+	.align 2
+	HelloTextPtr:   .word HelloText
+	ActionTextPtr:  .word ActionText
+
+	buffer:         .space  64
+	par:            .word   0
+			.word   buffer
+			.word   64
+
+.text
+	addi    r14, r0, HelloTextPtr
+	trap    5
+start:
+	addi    r14, r0, ActionTextPtr
+	trap    5
+
+	addi    r14, r0, par
+	trap    3
+
+	seqi    r1, par, 2
+	beqz    r1, start
+
+	; j     start
+
+	trap    0
