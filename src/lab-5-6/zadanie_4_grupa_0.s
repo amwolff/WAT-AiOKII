23fe86885f5aa577b2453c2d56f4797677b95a0d
Artur M. Wolff <artur.m.wolff@gmail.com>
2018-04-08 22:52:04 +0200

    Rewritten group 0 for ex. 4

--- src/lab-5-6/zadanie_4_grupa_0.s
+++ src/lab-5-6/zadanie_4_grupa_0.s
@@ -1,57 +1,38 @@
 .data
-	letters:        .asciiz "bcdefghijklm"
-	lettersNumber:  .word   97
-
 	helloText:      .asciiz "Witaj!\nProgram zostal napisany przez wyrobnika z grupy H7X2S1.\nMoj numer stanowiska: 404.\n"
 	actionText:     .asciiz "Prosze o podanie znaku:"
-	letterText:     .asciiz "%s\n"
+	outputChar:     .asciiz "\n%c\n"
+	readBuffer:     .space 64
+	buffer:         .word 0
+			.word readBuffer
+			.word 64
 
 	.align 2
-	helloTextPtr:   .word   helloText
-	actionTextPtr:  .word   actionText
-	letterTextPtr:  .word   letterText
-	letter:         .word   0
-
-	readBuffer:     .space  64
-	readPar:        .word   0
-			.word   readBuffer
-			.word   64
+	helloTextPtr:   .word helloText
+	actionTextPtr:  .word actionText
+	outputCharPtr:  .word outputChar
+	char:           .word 0
 
 .text
-	addi    r14, r0, helloTextPtr
+	add     r14, r0, helloTextPtr
+	trap    5
+try:
+	add     r14, r0, actionTextPtr
 	trap    5
 
-mainLoop:
-	addi    r14, r0, actionTextPtr
-	trap    5
-
-	addi    r14, r0, readPar
+	add     r14, r0, buffer
 	trap    3
 
-	seqi    r2, r1, 1
-	beqz    r2, mainLoop
+	add     r2, r0, readBuffer
 
-	lb      r3, readBuffer
+	lb      r3, 0(r2)
+	; grupa 0: b c d e f g h i j k l m -- 98 - 109
+	sgei    r4, r3, 98
+	slei    r5, r3, 109
+	and     r6, r4, r5
+	beqz    r6, try
 
-	lw      r4, lettersNumber
-
-	addi    r5, r0, 0
-
-letterCheckLoop:
-	lb      r6, letters(r5)
-
-	seqi    r7, r6, 10
-	bnez    r7, mainLoop
-
-	seq     r8, r3, r6
-	bnez    r8, break
-
-	addi    r4, r4, 1
-	addi    r5, r5, 1
-	j       letterCheckLoop
-
-break:
-	sw      letter, r3
-	addi    r14, r0, letterTextPtr
+	sw      char, r3
+	add     r14, r0, outputCharPtr
 	trap    5
-	j       mainLoop
+	j       try
